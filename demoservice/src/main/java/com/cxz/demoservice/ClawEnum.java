0f57fe4b026f749d299f6debec8694ef213fa952
Eric.Cui <465785032@qq.com>
2026-03-18 17:46:47 +0800

    test

--- demoservice/src/main/java/com/cxz/demoservice/ClawEnum.java
+++ demoservice/src/main/java/com/cxz/demoservice/ClawEnum.java
@@ -8,5 +8,11 @@ package com.cxz.demoservice;
  * @date 2022/11/1 15:25
  */
 public enum ClawEnum {
-    
+    XNK("XNK"),
+    JPMN("JPMN");
+
+    private String code;
+    ClawEnum(String code) {
+        this.code = code;
+    }
 }
